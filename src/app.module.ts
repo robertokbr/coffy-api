b6861ce3ab64f4d30203b1283a2bf03ea3a2d389
Roberto Junior <robertojuniordev@gmail.com>
2021-11-19 08:43:29 -0300

    feat: add orders module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, OrdersModule],
   controllers: [AppController],
   providers: [AppService],
 })
