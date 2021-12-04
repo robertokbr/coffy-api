76440cbca767c3bcf0b9271a0359b485a88be2e6
Roberto Junior <robertojuniordev@gmail.com>
2021-12-04 17:44:32 -0300

    feat: update module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { OrdersModule } from './orders/orders.module';
+import { UsersModule } from './modules/users/users.module';
+import { OrdersModule } from './modules/orders/orders.module';
+import { PrismaService } from './prisma/prisma.service';
+import { ItemsModule } from './modules/items/items.module';
 
 @Module({
-  imports: [UsersModule, OrdersModule],
+  imports: [UsersModule, OrdersModule, ItemsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
