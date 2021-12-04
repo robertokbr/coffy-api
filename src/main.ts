bcd5bdc8d543d47e124ced4acff8f8d8d07d4409
Roberto Junior <robertojuniordev@gmail.com>
2021-12-04 17:46:07 -0300

    feat: add validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { logger } from './shared/logger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  //pipes
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+
+  await app.listen(3000, () => {
+    logger.info({
+      message: 'App started on port 3000 🚀',
+      context: 'NestApplication',
+    });
+  });
 }
 bootstrap();
