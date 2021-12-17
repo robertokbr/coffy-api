67066dc3cc4668497e831d5894e6b7b86ffd6cb2
robertokbr <robertojuniordev@gmail.com>
2021-12-17 12:42:32 -0300

    chore: remove old modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
-import { OrdersModule } from './modules/orders/orders.module';
 import { PrismaService } from './prisma/prisma.service';
-import { ItemsModule } from './modules/items/items.module';
 
 @Module({
-  imports: [UsersModule, OrdersModule, ItemsModule],
+  imports: [UsersModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
