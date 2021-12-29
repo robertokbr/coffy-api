eff6d76b53ea64606e794311fea10c9886a8e1e4
robertokbr <robertojuniordev@gmail.com>
2021-12-29 12:32:46 -0300

    chore: remove old imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './modules/users/users.module';
-import { PrismaService } from './prisma/prisma.service';
+import { PrismaService } from './shared/prisma/prisma.service';
+import { OrdersModule } from './modules/orders/orders.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [OrdersModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { logger } from './shared/logger';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,18 @@ async function bootstrap() {
     }),
   );
 
+  const config = new DocumentBuilder()
+    .setTitle('Coffy API documentation')
+    .setVersion('1.0')
+    .setDescription(
+      'Coffy API is the API of the app Coffy made with react native, and located at my Github',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000, () => {
     logger.info({
       message: 'App started on port 3000 🚀',
@@ -20,4 +33,5 @@ async function bootstrap() {
     });
   });
 }
+
 bootstrap();
