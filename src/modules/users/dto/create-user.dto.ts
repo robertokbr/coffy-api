bd0162ae9a44a5d80774b1c10b1c85041d7303cc
robertokbr <robertojuniordev@gmail.com>
2021-12-29 12:32:29 -0300

    chore: remove users module

--- src/modules/users/dto/create-user.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsString, IsEmail } from 'class-validator';
-
-export class CreateUserDto {
-  @IsString()
-  name: string;
-
-  @IsEmail()
-  email: string;
-}
--- src/modules/users/entities/user.entity.ts
+++
@@ -1,7 +0,0 @@
-import { User as prismaUser } from '@prisma/client';
-
-export class User implements prismaUser {
-  id: number;
-  email: string;
-  name: string;
-}
