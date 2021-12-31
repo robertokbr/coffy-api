62c0551f30f8e45e61ccb6801c493759d29ab8c5
robertokbr <robertojuniordev@gmail.com>
2021-12-31 14:51:29 -0300

    feat: add types

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -1,1 +1,19 @@
+import { Observable } from 'rxjs';
+
 export type JsonObject = { [key: string]: string };
+
+export interface AuthService {
+  createPassCode(data: any): Observable<{
+    code: string;
+    createdAt: string;
+  }>;
+  getSessionPayload(data: { jwt: string }): Observable<{
+    id: string;
+    name: string;
+    code: string;
+  }>;
+  createSession(data: { code: string; name: string }): Observable<{
+    jwt: string;
+    expiration: string;
+  }>;
+}
