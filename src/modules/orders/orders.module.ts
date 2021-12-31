20d5ee59515317909583c6b3428cf71000b9885a
robertokbr <robertojuniordev@gmail.com>
2021-12-31 14:52:17 -0300

    feat: add grpc clients module & configure middleware

--- src/modules/orders/orders.module.ts
+++ src/modules/orders/orders.module.ts
@@ -1,9 +1,41 @@
-import { Module } from '@nestjs/common';
-import { OrdersService } from './orders.service';
-import { OrdersController } from './orders.controller';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
+import { OrdersService } from './services/orders.service';
+import { OrdersController } from './controllers/orders.controller';
+import { OrdersRepository } from './orders.repository';
+import { PassCodesController } from './controllers/pass-codes.controller';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import paths from 'src/configs/paths';
+import { SessionsController } from './controllers/sessions.controller';
+import EnsureAuthenticated from './middlewares/ensure-authenticated';
+import { ItemsRepository } from './items.repository';
+import { ItemsController } from './controllers/items.controller';
 
 @Module({
-  controllers: [OrdersController],
-  providers: [OrdersService],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'AuthService',
+        transport: Transport.GRPC,
+        options: {
+          url: '0.0.0.0:50051',
+          package: 'Auth',
+          protoPath: paths.proto,
+        },
+      },
+    ]),
+  ],
+  controllers: [
+    OrdersController,
+    PassCodesController,
+    SessionsController,
+    ItemsController,
+  ],
+  providers: [OrdersService, OrdersRepository, ItemsRepository],
 })
-export class OrdersModule {}
+export class OrdersModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(EnsureAuthenticated)
+      .forRoutes({ path: 'orders', method: RequestMethod.POST });
+  }
+}
