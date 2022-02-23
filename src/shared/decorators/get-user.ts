9688942620d7d8c6afe1c17e91c1d9ed313bea19
robertokbr <robertojuniordev@gmail.com>
2022-02-23 19:25:36 -0300

    refactor: change decorator return type

--- src/shared/decorators/get-user.ts
+++ src/shared/decorators/get-user.ts
@@ -1,8 +1,8 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { User } from '../../modules/orders/entities/user.entity';
+import { UserDto } from 'src/modules/orders/dto/user.dto';
 
 export const GetUser = createParamDecorator(
-  async (_data, context: ExecutionContext): Promise<User> => {
+  async (_data, context: ExecutionContext): Promise<UserDto> => {
     const req = context.switchToHttp().getRequest();
     return req.user;
   },
