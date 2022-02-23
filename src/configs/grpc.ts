79ba607607f1bb2a26e6fb1ecd6f230ba2d1b804
robertokbr <robertojuniordev@gmail.com>
2022-02-23 19:24:51 -0300

    refactor: update grpc config

--- src/configs/grpc.ts
+++ src/configs/grpc.ts
@@ -5,12 +5,12 @@ interface IgRPCConfigs {
   authService: ClientProviderOptions;
 }
 
-export default {
+export const grpcConfigs = {
   authService: {
-    name: 'AuthService',
+    name: 'AuthServiceProvider',
     transport: Transport.GRPC,
     options: {
-      url: '0.0.0.0:50051',
+      url: process.env.AUTH_SERVICE_PROVIDER_URL,
       package: 'Auth',
       protoPath: paths.proto,
     },
